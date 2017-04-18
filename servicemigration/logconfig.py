0ad8e21428afc540c66384a7fb45552c38a07abf
Young Lee <ylee@zenoss.com>
2017-04-18 09:35:08 -0500

    Use upper camel case for property names

--- servicemigration/logconfig.py
+++ servicemigration/logconfig.py
@@ -4,12 +4,13 @@ import logtag
 
 
 default = {
-    "path": "",
-    "type": "",
-    "filters": [],
+    "Path": "",
+    "Type": "",
+    "Filters": [],
     "LogTags": [
         copy.deepcopy(logtag.default),
         ],
+    "IsAudit": True,
 }
 
 
@@ -23,10 +24,11 @@ def deserialize(data):
     for d in data:
         lc = LogConfig()
         lc._LogConfig__data = d
-        lc.path = d.get("path", d.get("Path"))
-        lc.logType = d.get("type", d.get("Type"))
-        lc.filters = d.get("filters", d.get("Filters"))
-        lc.logTags = logtag.deserialize(d.get("LogTags", []))
+        lc.path = d.get("Path", default["Path"])
+        lc.logType = d.get("Type", default["Type"])
+        lc.filters = d.get("Filters", default["Filters"])
+        lc.logTags = logtag.deserialize(d.get("LogTags", default["LogTags"]))
+        lc.isAudit = d.get("IsAudit", default["IsAudit"])
         logconfigs.append(lc)
     return logconfigs
 
@@ -38,10 +40,11 @@ def serialize(logconfigs):
     data = []
     for lc in logconfigs:
         d = copy.deepcopy(lc._LogConfig__data)
-        d["path"] = lc.path
-        d["type"] = lc.logType
-        d["filters"] = lc.filters
+        d["Path"] = lc.path
+        d["Type"] = lc.logType
+        d["Filters"] = lc.filters
         d["LogTags"] = logtag.serialize(lc.logTags)
+        d["IsAudit"] = lc.isAudit
         data.append(d)
     return data
 
@@ -51,9 +54,10 @@ class LogConfig(object):
     A collection of path and type info, filters, and tags.
     """
 
-    def __init__(self, path="", logType="", filters=None, logTags=None):
+    def __init__(self, path="", logType="", filters=None, logTags=None, isAudit=True):
         self.__data = copy.deepcopy(default)
         self.path = path
         self.logType = logType
         self.filters = filters or []
         self.logTags = logTags or []
+        self.isAudit = isAudit
